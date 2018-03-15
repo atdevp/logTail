90f3c8b07f74f5d271ed79fadc6e33c0731cb847
jiangleiyu <jiangleiyu@sohu-inc.com>
2018-03-15 19:00:03 +0800

    规范

--- log-shiper.go
+++ log-shiper.go
@@ -11,8 +11,8 @@ import (
 
 
 var (
-	filename = kingpin.Arg("filename", "log file").Required().String()
-	topic  = kingpin.Arg("topic", "topic name").Required().String()
+    filename = kingpin.Arg("filename", "log file").Required().String()
+    topic  = kingpin.Arg("topic", "topic name").Required().String()
     brokers = kingpin.Arg("brokers", "kafka brokers").Required().String()
     httpport = kingpin.Arg("httpport", "http port").Required().String()
     listentip = kingpin.Arg("listentip", "listen ip address").Required().String()
